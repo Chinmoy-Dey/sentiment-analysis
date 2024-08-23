07dc43220544fc51859aca8e1569be379e193f8f
Chinmoy Dey <chinmoy2004@gmail.com>
2024-08-23 18:28:33 +0530

    test and Action

--- sentiment_analysis_project/tests/test_predictions.py
+++ sentiment_analysis_project/tests/test_predictions.py
@@ -7,14 +7,25 @@ file = Path(__file__).resolve()
 parent, root = file.parent, file.parents[1]
 sys.path.append(str(root))
 
+import pytest
 import numpy as np
 from sklearn.metrics import mean_squared_error, r2_score
 
-from sentiment_analysis.predict import make_prediction
+from sentiment_analysis_model.predict import analyze_sentiment
+
+@pytest.fixture
+def positive_sentiment():
+  return "Positive News is a constructive journalism media brand.We love positive News"
+
+@pytest.fixture
+def negative_sentiment():
+  return "This assignment was tough as there are many open pieces to make it work together!"
+
+def test_negative_sentiment(negative_sentiment):
+  result = analyze_sentiment(negative_sentiment)
+  assert result['sentiment'] == 'NEGATIVE'
 
 
-def test_make_prediction(sample_input_data):
-    # Test the function with a sample text
-    sample_text = "I dont love this assignemt!"
-    output = analyze_sentiment(sample_text)
-    print (output)
+def test_positive_sentiment(positive_sentiment):
+  result = analyze_sentiment(positive_sentiment)
+  assert result['sentiment']  == 'POSITIVE'
